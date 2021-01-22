273271e9ec41d9a74437ec1fe6d873572e6b2ae8
yunhalee05 <yunhalee0506@naver.com>
2021-01-22 20:13:03 +0900

    add annotation

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ class Calculator{
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
     }
-
+  
     clear(){ /*AC 버튼 눌렀을 때*/
         this.currentOperand = ''
         this.previousOperand = ''
         this.poreation = undefined
     }
-
+  
     delete(){/*del 버튼 눌렀을 때*/
         this.currentOperand = this.currentOperand.toString().slice(0, -1) /*한자리 삭제(가장 최근것을 삭제) */
     }
-
+  
     appendNumber(number){/* 숫자버튼 눌렀을 때*/
         if(number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
-
+  
     chooseOperation(operation){ /* 수식기호 버튼 눌렀을 때*/
         if(this.currentOperand ==='') return
         if(this.previousOperand !== ''){
@@ -30,7 +30,7 @@ class Calculator{
         this.currentOperand = ''
 
     }
-
+  
     compute(){/* = 버튼 눌렀을 때*/
         let computation
         const prev = parseFloat(this.previousOperand)
@@ -57,6 +57,7 @@ class Calculator{
         this.operation = undefined
         this.previousOperand = ''
     }
+  
     getDisplayNumber(number){ /*숫자가 여러개 눌릴때 숫자를 어떻게 보일지 설정*/
         const stringNumber = number.toString()
         const integerDigits = parseFloat(stringNumber.split('.')[0]) /* . 기준으로 앞에거 저장*/
@@ -65,8 +66,7 @@ class Calculator{
         if(isNaN(integerDigits)) {/*인티저자리는 en을 통해서 3자리마다 , 뜨도록 설정*/
             integerDisplay = ''
         }else{
-            integerDisplay = integerDigits.toLocaleString('en',{ 
-                maximumFractionDigits: 0})
+            integerDisplay = integerDigits.toLocaleString('en',{ maximumFractionDigits: 0})
         }
         if(decimalDigits !=null){/*소숫점자리는 ,붙지 않도록 설정*/
             return `${integerDisplay}.${decimalDigits}`
@@ -75,61 +75,56 @@ class Calculator{
         }
 
     }
-
+  
     updateDisplay(){/* 화면에 어떻게 보일 지 설정한다.*/
         this.currentOperandTextElement.innerText = 
-        this.getDisplayNumber(this.currentOperand)
+            this.getDisplayNumber(this.currentOperand)
         if(this.operation != null) { /*수식기호가 있다면 계산한 값을 previousOperand에 보이도록 한다.*/
             this.previousOperandTextElement.innerText = 
-            `${this.getDisplayNumber(previousOperand)} ${this.operation}`
-        } else{/*없으면 current창만 뜨도록 설정  */
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {/*없으면 current창만 뜨도록 설정  */
             this.previousOperandTextElement.innerText = ''
 
         }
     }
 }
-
-
-
-
-
-const numberButtons=document.querySelectorAll('[data-number]')
-const operationButtons=document.querySelectorAll('[data-operation]')
-const equalsButton=document.querySelector('[data-equals]')
-const deleteButton=document.querySelector('[data-delete]')
-const allClearButton=document.querySelector('[data-all-clear]')
-const previousOperandTextElement=document.querySelector('[data-previous-operand]')
-const currentOperandTextElement=document.querySelector('[data-current-operand]')
-
-const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
-
-
-/*각 버튼을 클릭했을때 어떻게 작동할 것인지 설정 */
-numberButtons.forEach(button => {
-    button.addEventListener('click', ()=>{
-        calculator.appendNumber(button.innerText)
-        calculator.updateDisplay()
+  
+  
+  const numberButtons = document.querySelectorAll('[data-number]')
+  const operationButtons = document.querySelectorAll('[data-operation]')
+  const equalsButton = document.querySelector('[data-equals]')
+  const deleteButton = document.querySelector('[data-delete]')
+  const allClearButton = document.querySelector('[data-all-clear]')
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+  const currentOperandTextElement = document.querySelector('[data-current-operand]')
+  
+  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  
+  numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
     })
-})
-operationButtons.forEach(button => {
-    button.addEventListener('click', ()=>{
-        calculator.chooseOperation(button.innerText)
-        calculator.updateDisplay()
+  })
+  
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
     })
-})
-
-equalsButton.addEventListener('click' , button =>{
+  })
+  
+  equalsButton.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
-})
-
-allClearButton.addEventListener('click' , button =>{
+  })
+  
+  allClearButton.addEventListener('click', button => {
     calculator.clear()
     calculator.updateDisplay()
-})
-
-deleteButton.addEventListener('click' , button =>{
+  })
+  
+  deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
-})
-
+  })
